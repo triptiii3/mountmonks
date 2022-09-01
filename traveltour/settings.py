30a3d4674abe182ae4188edcb1d0447f0f57c177
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-09-01 07:38:57 +0530

    any commit

--- traveltour/settings.py
+++ traveltour/settings.py
@@ -132,6 +132,7 @@ MEDIA_URL = '/images/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT = (
     os.path.join(BASE_DIR,'static')
 )
--- traveltour/urls.py
+++ traveltour/urls.py
@@ -62,4 +62,4 @@ urlpatterns = [
 
    
     
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
