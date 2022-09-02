b4640f526e32c54306cdf700f13054d534a2004b
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-09-02 08:05:42 +0530

    changeddd

--- travel/settings.py
+++ travel/settings.py
@@ -90,7 +90,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+import dj_database_url
+db_from_env=dj_database_url.config(con_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
