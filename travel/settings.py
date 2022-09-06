ab457410cf11e85914ef0f8513d5fbe64c710511
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-09-06 10:57:15 +0530

    changes

--- travel/settings.py
+++ travel/settings.py
@@ -13,8 +13,6 @@ import os
 from django.contrib.messages import constants as messages
 import dj_database_url
 from pathlib import Path
-import cloudinary
-import cloudinary_storage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,8 +40,7 @@ INSTALLED_APPS = [
     'home',
     'destinations',
     
-    'cloudinary',
-    'cloudinary_storage',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     
@@ -55,7 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-  
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     
     
@@ -137,16 +134,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-from decouple import config
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('dwdwjc45e', default=""),
-    'API_KEY': config('962262553439632', default=""),
-    'API_SECRET': config('Y-pQxwdsMRU2So_Oy31ecHLueFQ', default=""),
-}
-
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MESSAGE_TAGS ={
     messages.ERROR:'danger'
 }
