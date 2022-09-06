4502d97022b1f9f239b62b978043f2b1cd31f0ff
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-09-06 11:18:56 +0530

    mains

--- travel/settings.py
+++ travel/settings.py
@@ -135,7 +135,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 MESSAGE_TAGS ={
     messages.ERROR:'danger'
 }
