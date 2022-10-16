de5eacc012b0bd453badea5aae575ae20d4c4f0c
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-10-16 11:08:01 +0530

    settinga

--- travel/settings.py
+++ travel/settings.py
@@ -25,11 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0zn*7p4ox!k*j@$se+vvf^i0frl-)&nz^^*d1#00a+3jzpuqsv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ '0.0.0.0',
-    'mountmonks.herokuapp.com', # your herokuapp url
-    '127.0.0.1']
+ALLOWED_HOSTS = [ '*']
 
 
 # Application definition
